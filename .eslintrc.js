353ea89a583ad44e6b51d479c396f21f5b4f7bd8
Victor Vuelma <victorvuelma@outlook.com>
2019-11-18 18:00:29 -0300

    prettier config and fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,8 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -15,11 +14,15 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    "camelcase": "off",
-    "no-unused-vars": ["error", {
-      "argsIgnorePattern": "next"
-    }]
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: 'next',
+      },
+    ],
   },
 };
