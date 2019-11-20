c30445ea7cdf7bb7734208dfcb7a5ef8f694291f
Victor Vuelma <victorvuelma@outlook.com>
2019-11-20 15:05:53 -0300

    add route to update user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -7,9 +7,7 @@ class UserController {
     });
 
     if (userExists) {
-      return res.status(400).json({
-        error: 'User already exists.',
-      });
+      return res.status(400).json({ error: 'User already exists.' });
     }
 
     const { id, name, email, provider } = await User.create(req.body);
@@ -18,7 +16,32 @@ class UserController {
   }
 
   async update(req, res) {
-    return res.json({ ok: true });
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Passoword does not match.' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
