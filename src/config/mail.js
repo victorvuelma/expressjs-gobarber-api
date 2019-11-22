3bdbd39acd5c1821671c0b078b8562218a6d9e15
Victor Vuelma <victorvuelma@outlook.com>
2019-11-22 13:38:48 -0300

    add mail lib

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '04f77a78e2a46f',
+    pass: '1a0ecb9b57976d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
